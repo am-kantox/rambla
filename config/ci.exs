9770d7e66d0c36ab93a2dc4a1b90cc2566e6f89a
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-08-16 08:42:18 +0200

    Added image for RabbitMQ CI

--- config/ci.exs
+++ config/ci.exs
@@ -3,7 +3,7 @@ import Config
 config :rambla,
   redis: [
     host: "127.0.0.1",
-    port: System.get_env("REDIS_PORT", 6379),
+    port: String.to_integer(System.get_env("REDIS_PORT", "6379")),
     password: "",
     db: 0,
     reconnect: 1_000,
@@ -12,7 +12,7 @@ config :rambla,
   rabbitmq: [
     host: "localhost",
     password: "guest",
-    port: System.get_env("RABBITMQ_PORT", 5672),
+    port: String.to_integer(System.get_env("RABBITMQ_PORT", "5672")),
     username: "guest",
     virtual_host: "/",
     x_message_ttl: "4000"
