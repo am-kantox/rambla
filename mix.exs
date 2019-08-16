74bfc232d9b3f5cec50c449619f852444f4594b9
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-08-16 08:47:37 +0200

    Added image for RabbitMQ CI

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Rambla.MixProject do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  # defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  defp elixirc_paths(:ci), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   defp aliases do
