66e922e642ddb56ad42a7e9da4f37284c49b9884
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-08-19 08:07:18 +0200

    Dialyzer fixes.

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule Rambla.MixProject do
       docs: docs(),
       dialyzer: [
         plt_file: {:no_warn, ".dialyzer/plts/dialyzer.plt"},
+        plt_add_apps: [:amqp, :exredis],
         ignore_warnings: ".dialyzer/ignore.exs"
       ]
     ]
