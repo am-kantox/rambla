af41d6bd0c5238b93766e5dbf2db7125fdae0f39
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-01-24 14:21:12 +0100

    GH Action

--- config/test.exs
+++ config/test.exs
@@ -2,18 +2,18 @@ import Config
 
 config :rambla,
   redis: [
-    host: "127.0.0.1",
-    port: 6379,
-    password: "",
+    host: System.get_env("REDIS_HOST", "127.0.0.1"),
+    port: String.to_integer(System.get_env("REDIS_PORT", "6379")),
+    password: System.get_env("REDIS_PASSWORD", ""),
     db: 0,
     reconnect: 1_000,
     max_queue: :infinity
   ],
   rabbitmq: [
-    host: "localhost",
-    password: "guest",
-    port: 5672,
-    username: "guest",
-    virtual_host: "/",
+    host: System.get_env("RABBITMQ_HOST", "127.0.0.1"),
+    password: System.get_env("RABBITMQ_PASSWORD", "guest"),
+    port: String.to_integer(System.get_env("RABBITMQ_PORT", "5672")),
+    username: System.get_env("RABBITMQ_USERNAME", "guest"),
+    virtual_host: System.get_env("RABBITMQ_VHOST", "/"),
     x_message_ttl: "4000"
   ]
